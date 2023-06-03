a4aa1d7f496c9394ac97462e359394134dd80f30
yuval2313 <lyuval1210@gmail.com>
2023-06-03 22:21:50 +0300

    clientApi: Created client microservice
    Created initial template for clientApi microservice with profiles resource.

--- client/src/profiles/profiles.module.ts
+++ client/src/profiles/profiles.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProfilesService } from './profiles.service';
 import { ProfilesController } from './profiles.controller';
+import { Profile } from './models/profile.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Profile])],
   controllers: [ProfilesController],
-  providers: [ProfilesService]
+  providers: [ProfilesService],
 })
 export class ProfilesModule {}
--- client/src/profiles/profiles.service.ts
+++ client/src/profiles/profiles.service.ts
@@ -1,26 +1,82 @@
 import { Injectable } from '@nestjs/common';
-import { CreateProfileDto } from './dto/create-profile.dto';
-import { UpdateProfileDto } from './dto/update-profile.dto';
+import { CreateProfileDto } from './req-dtos/create-profile.dto';
+import { UpdateProfileDto } from './req-dtos/update-profile.dto';
+import { Profile } from './models/profile.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
+import { CConflictException } from '../errors/conflict.error';
+import { CNotFoundException } from '../errors/not-found.error';
 
 @Injectable()
 export class ProfilesService {
-  create(createProfileDto: CreateProfileDto) {
-    return 'This action adds a new profile';
+  constructor(
+    @InjectPinoLogger(ProfilesService.name)
+    private readonly logger: PinoLogger,
+    @InjectRepository(Profile)
+    private readonly profilesRepository: Repository<Profile>,
+  ) {}
+
+  async createProfile(createProfileDto: CreateProfileDto): Promise<Profile> {
+    this.logger.info('Creating new profile entity');
+
+    this.logger.info('Verifying user id uniqueness');
+    const { user_id } = createProfileDto;
+    const existingUserId = await this.profilesRepository.findOneBy({
+      user_id,
+    });
+    if (existingUserId)
+      throw new CConflictException(
+        'Profile with given user id already exists',
+        { userId: user_id },
+      );
+
+    const newProfile = this.profilesRepository.create(createProfileDto);
+    const profile = await this.profilesRepository.save(newProfile);
+
+    this.logger.info('Successfully created profile enitity');
+    return profile;
   }
 
-  findAll() {
-    return `This action returns all profiles`;
+  async findOneByUserId(userId: number): Promise<Profile> {
+    this.logger.info('Retrieving profile by user id');
+    const profile = await this.profilesRepository.findOneBy({
+      user_id: userId,
+    });
+
+    if (!profile)
+      throw new CNotFoundException(
+        'Profile with the given user id was not found',
+        { userId },
+      );
+
+    this.logger.info('Successfully retrieved profile by user id');
+    return profile;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} profile`;
+  async updateOneByUserId(
+    userId: number,
+    updateProfileDto: UpdateProfileDto,
+  ): Promise<Profile> {
+    this.logger.info('Updating profile entity by user id');
+
+    const profile = await this.findOneByUserId(userId);
+
+    Object.assign(profile, updateProfileDto);
+
+    const updatedProfile = await this.profilesRepository.save(profile);
+
+    this.logger.info('Successfully updated profile entity');
+    return updatedProfile;
   }
 
-  update(id: number, updateProfileDto: UpdateProfileDto) {
-    return `This action updates a #${id} profile`;
-  }
+  async removeOneByUserId(userId: number): Promise<Profile> {
+    this.logger.info('Removing profile entity by user id');
 
-  remove(id: number) {
-    return `This action removes a #${id} profile`;
+    const profile = await this.findOneByUserId(userId);
+    const removedUser = await this.profilesRepository.softRemove(profile);
+
+    this.logger.info('Successfully removed profile entity');
+    return removedUser;
   }
 }
